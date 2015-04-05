0b3b4decb531f8586133fc6a13dc9e365e65ad84
Sabri <sabri@mykolab.com>
2015-04-05 18:37:24 +0200

    Few improvements
    - Added constraints
    - Fullscreen mode activated
    - Added rounded borders at the bottom

---
+++ MainSplitViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainSplitViewController.h
+//  Tox
+//
+//  Created by Sabri on 05/04/15.
+//  Copyright (c) 2015 Tox Foundation. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MainSplitViewController : NSSplitViewController
+
+@end
--- MainSplitViewController.m
+++ MainSplitViewController.m
@@ -1,36 +1,37 @@
 //
-//  ViewController.m
+//  MainSplitViewController.m
 //  Tox
 //
 //  Created by Sabri on 05/04/15.
 //  Copyright (c) 2015 Tox Foundation. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "MainSplitViewController.h"
 
-@implementation ViewController
+@implementation MainSplitViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
+    
+    NSLog(@"called from ToxSplitViewController/viewDidLoad");
     // Do any additional setup after loading the view.
-}
-
-- (void)setRepresentedObject:(id)representedObject {
-    [super setRepresentedObject:representedObject];
-
-    // Update the view, if already loaded.
+    
 }
 
 - (void)viewDidAppear {
     //[super viewDidAppear];
     
-    NSLog(@"test");
+    NSLog(@"called from ToxSplitViewController/viewDidAppear");
     
     // Fix: Status bar
     self.view.window.titlebarAppearsTransparent = true;
     self.view.window.movableByWindowBackground = true;
     self.view.window.styleMask = self.view.window.styleMask | NSFullSizeContentViewWindowMask;
 }
+
+/*- (CGFloat)splitView:(NSSplitView *)splitView constrainMinCoordinate:(CGFloat)proposedMin ofSubviewAt:(NSInteger)dividerIndex {
+    NSLog(@"%ld", (long)dividerIndex);
+    return proposedMin;
+}*/
 
 @end
